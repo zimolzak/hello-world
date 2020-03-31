62a8c05892600c3a0536fec34d6c0ceb4095fd9a
Andy Zimolzak (2018 mbp) <andyzimolzak@gmail.com>
2020-03-31 09:22:39 -0500

    Most concise, and no space before newline.

--- alexs.py
+++ alexs.py
@@ -3,6 +3,4 @@ N_REPS = 3
 LETTERS = 'A L E X S'.split()
 
 for i in range(N_ROWS):
-    for L in LETTERS:
-        print(L * 3 + ' ', end='')
-    print()
+    print(' '.join([lett * N_REPS for lett in LETTERS]))
