6277788fe6d3e9fd07c476b4692a04e20395dcc3
Andy Zimolzak (2018 mbp) <andyzimolzak@gmail.com>
2020-03-31 09:17:38 -0500

    Better var names

--- alexs.py
+++ alexs.py
@@ -1,8 +1,11 @@
-lett = 'A L E X S'.split()
+N_ROWS = 10
+N_REPS = 3
 
-for i in range(10):
-    for L in lett:
-        for j in range(3):
+LETTERS = 'A L E X S'.split()
+
+for i in range(N_ROWS):
+    for L in LETTERS:
+        for j in range(N_REPS):
             print(L, end='')
         print(' ', end='')
     print()
