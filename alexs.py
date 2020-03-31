e418446c6a82f1bc25088666a132f7a3230e0a14
Andy Zimolzak (2018 mbp) <andyzimolzak@gmail.com>
2020-03-31 09:19:47 -0500

    Won't be needing this loop.

--- alexs.py
+++ alexs.py
@@ -1,11 +1,8 @@
 N_ROWS = 10
 N_REPS = 3
-
 LETTERS = 'A L E X S'.split()
 
 for i in range(N_ROWS):
     for L in LETTERS:
-        for j in range(N_REPS):
-            print(L, end='')
-        print(' ', end='')
+        print(L * 3 + ' ', end='')
     print()
